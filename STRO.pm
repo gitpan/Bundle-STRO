7671f6be78281b9c4f8a4b8b22ef6df0c73df231
Serguei Trouchelle <stro@cpan.org>
2008-06-23 08:40:37 +0000

    Import of STRO/Bundle-STRO-1.01 from CPAN.
    gitpan-cpan-distribution: Bundle-STRO
    gitpan-cpan-version:      1.01
    gitpan-cpan-path:         STRO/Bundle-STRO-1.01.tar.gz
    gitpan-cpan-author:       STRO
    gitpan-cpan-maturity:     released

---
+++ STRO.pm
@@ -0,0 +1,47 @@
+package Bundle::STRO;
+
+our $VERSION = "1.01";
+
+1;
+
+=head1 NAME
+
+Bundle::STRO
+
+=head1 SYNOPSIS
+
+ perl -MCPAN -e 'install Bundle::STRO'
+
+=head1 CONTENTS
+
+Data::Define
+
+FindBin::Real
+
+FTN::Address
+
+FTN::Nodelist
+
+Lingua::Cyrillic::Translit::ICAO
+
+Lingua::Multinational::Translit::ICAO
+
+Lingua::RU::Jcuken
+
+Lingua::EN::Jcuken
+
+Prompt::Timeout
+
+Text::Compare
+
+Win32::Uptime
+
+=head1 DESCRIPTION
+
+Bundle of modules I maintain.
+
+=head1 AUTHOR
+
+Serguei Trouchelle E<lt>F<stro@railways.dp.ua>E<gt>
+
+=cut 
